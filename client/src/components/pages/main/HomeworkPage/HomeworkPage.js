253a61a8996a7ccbf2b34feb0d6a5d67f3b6a5d8
klevcsoo <kevelajsz@outlook.com>
2019-10-11 23:58:16 +0200

    Completed homework loading

--- client/src/components/pages/main/HomeworkPage/HomeworkPage.js
+++ client/src/components/pages/main/HomeworkPage/HomeworkPage.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react'
 import Routes from '../../../../constants/routes'
+import FirebaseHandler from '../../../../FirebaseHandler'
 
 // Components
 import MainPageLayout from '../../../layout/MainPageLayout'
 import AppPopup from '../../../AppPopup/AppPopup'
 import AppButton from '../../../AppButton/AppButton'
 import AppDivider from '../../../AppDivider'
+import AppCardClassContent from '../../../AppCard/AppCardClassContent'
+import LoadingSpinner from '../../../LoadingSpinner'
 
 export class HomeworkPage extends Component {
   state = {
     popupVisible: false,
-    popupMessage: ''
+    popupMessage: '',
+    homework: []
+  }
+
+  componentDidMount() {
+    FirebaseHandler.getClientInfo((result) => {
+      let classId = result.classId;
+      FirebaseHandler.readDataContinuously(`/classes/${classId}/homework`, (snapshot) => {
+        snapshot.forEach((homeworkSnapshot) => {
+          let hw = homeworkSnapshot.val();
+          hw.id = homeworkSnapshot.key;
+          this.setState((state) => {
+            let updatedState = state;
+            updatedState.homework.push(hw);
+            return updatedState;
+          });
+        });
+      });
+    });
   }
 
   render() {
@@ -19,7 +40,12 @@ export class HomeworkPage extends Component {
         {this.state.popupVisible ? <AppPopup message={this.state.popupMessage} onClose={this.hidePopup} /> : null }
         <AppButton type="homework" text="Hozzáadás" onClick={() => {this.props.history.push(Routes.HOMEWORK_ADD)}} />
         <AppDivider />
-        <div></div>
+        <div>
+          {this.state.homework.length === 0 ? <LoadingSpinner /> : null}
+          {this.state.homework.map((hw) => (
+            <AppCardClassContent type="homework" {...hw} />
+          ))}
+        </div>
       </MainPageLayout>
     )
   }
