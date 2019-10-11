eced2b457e422d9d4e55d7aeb782dd191ea1d6a8
klevcsoo <kevelajsz@outlook.com>
2019-10-11 23:04:40 +0200

    Created MainPageLayout

--- client/src/components/layout/MainPageLayout.js
+++ client/src/components/layout/MainPageLayout.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
+import FirebaseHandler from '../../FirebaseHandler'
+import Routes from '../../constants/routes'
+import PageTitle from './PageTitle/PageTitle';
+import PageNavbar from './PageNavbar/PageNavbar';
 
 export class MainPageLayout extends Component {
+  componentDidMount() {
+    FirebaseHandler.getApp().auth().onAuthStateChanged((user) => {
+      if (!user) this.props.history.push(Routes.LOGIN);
+    });
+  }
+
   render() {
     return (
       <React.Fragment>
-        
+        <PageTitle title={this.props.pageTitle} history={this.props.history} />
+        <PageNavbar active={this.props.pageActive} history={this.props.history} />
+        <div style={{ height: 80 }}></div>
+        {this.props.children}
+        <div style={{ height: 80 }}></div>
       </React.Fragment>
     )
   }
