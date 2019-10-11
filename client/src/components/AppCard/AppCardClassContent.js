212b3fc131dff1d961ce3814c6a232850bba3ad2
klevcsoo <kevelajsz@outlook.com>
2019-10-11 23:52:36 +0200

    Made AppCardClassContent type aware

--- client/src/components/AppCard/AppCardClassContent.js
+++ client/src/components/AppCard/AppCardClassContent.js
@@ -1,23 +1,16 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import './AppCard.css'
 
 export class AppCardClassContent extends Component {
   render() {
     return (
-      <div className="app-card app-card-class-content app-card-exam">
+      <div className={`app-card app-card-class-content app-card-${this.props.type}`}>
         <h1 id="content-title">{this.props.title}</h1>
         <p>Tantárgy: <span id="content-subject">{this.props.subject}</span></p>
         <p>Időpont: <span id="content-date">{this.props.date}</span></p>
       </div>
     )
   }
-}
-
-AppCardClassContent.propTypes = {
-  title: PropTypes.string.isRequired,
-  subject: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired
 }
 
 export default AppCardClassContent
