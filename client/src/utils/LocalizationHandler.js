026db89297213a037f031aad4aeef54aefd55549
klevcsoo <kevelajsz@outlook.com>
2019-10-12 16:17:34 +0200

    Created LocalizationHandler

---
+++ client/src/utils/LocalizationHandler.js
@@ -0,0 +1,19 @@
+class LocalizationHandler {
+  static formatDate(date) {
+    const innerDate = new Date(date);
+    const months = [
+        'január', 'február', 'március',
+        'április', 'május', 'június',
+        'július', 'augusztus', 'szeptember',
+        'október', 'november', 'december'
+    ];
+  
+    const year = innerDate.getFullYear();
+    const month = months[innerDate.getMonth()];
+    const day = innerDate.getDate();
+  
+    return `${year}. ${month} ${day}.`;
+  }
+}
+
+export default LocalizationHandler;
