381a9e7d92b810a5d14085e843f84773b0603b51
klevcsoo <kevelajsz@outlook.com>
2019-10-14 17:34:31 +0200

    Created Class Settings page

---
+++ client/src/components/pages/class/ClassSettingsPage/ClassSettings.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ClassSettings extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default ClassSettings
