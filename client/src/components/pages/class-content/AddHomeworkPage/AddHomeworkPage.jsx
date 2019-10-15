42e9dc6a3098e3c46a73c47183ddc1be02923e2e
klevcsoo <kevelajsz@outlook.com>
2019-10-15 09:18:34 +0200

    Began work on AddHomeworkPage

--- client/src/components/pages/class-content/AddHomeworkPage/AddHomeworkPage.jsx
+++ client/src/components/pages/class-content/AddHomeworkPage/AddHomeworkPage.jsx
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
 
+// Components
+import SaveablePageLayout from '../../../layout/SaveablePageLayout';
+import AppSubtitle from '../../../AppSubtitle';
+import AppCardClassContent from '../../../AppCard/AppCardClassContent';
+import AppInput from '../../../AppInput/AppInput';
+
 export class AddHomeworkPage extends Component {
+  state = {
+    currentHomework: {
+      title: null,
+      date: null,
+      subject: null
+    }
+  }
+
+  saveHomework = () =>  {
+
+  }
+
   render() {
     return (
-      <div>
-        
-      </div>
+      <SaveablePageLayout onSave={this.saveHomework}>
+        <AppSubtitle text="Előnézet:" />
+        <AppCardClassContent type="homework" {...this.state.currentHomework} />
+        <AppSubtitle text="Beállítások:" />
+        <div>
+          <AppInput placeholder="Házi feladat címe" text={this.state.currentHomework.title} onTextChanged={} />
+        </div>
+      </SaveablePageLayout>
     )
   }
 }
