12c50f15ee9466aa26830b87358593639f53cf3c
klevcsoo <kevelajsz@outlook.com>
2019-10-16 16:42:35 +0200

    Created AppDropDown

---
+++ client/src/components/AppDropDown/AppDropDown.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import AppColours from '../../constants/appColors'
+
+export class AppDropDown extends Component {
+  render() {
+    return (
+      <div style={{ width: 'fit-content', margin: '5px auto' }}>
+        <select style={{
+          width: 290,
+          height: 40,
+          padding: '0 15px',
+          background: AppColours.LIGHT,
+          border: 'none',
+          borderRadius: 20,
+          fontFamily: '"Roboto", sans-serif',
+          fontSize: 20
+        }}>
+          {this.props.children}
+        </select>
+      </div>
+    )
+  }
+}
+
+export default AppDropDown
