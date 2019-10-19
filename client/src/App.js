874cefd41579bfc69243d852655161346876f0e7
klevcsoo <kevelajsz@outlook.com>
2019-10-19 15:01:29 +0200

    Added ClassJoinPage

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,7 @@ import LoginOptionsPage from './components/pages/user/LoginOptionsPage';
 import DeleteAccountPage from './components/pages/user/DeleteAccountPage';
 import AddHomeworkPage from './components/pages/class-content/AddHomeworkPage/AddHomeworkPage';
 import AddExamPage from './components/pages/class-content/AddExamPage/AddExamPage';
+import ClassJoinPage from './components/pages/class/ClassJoinPage/ClassJoinPage';
 
 export class App extends Component {
   render() {
@@ -33,7 +34,7 @@ export class App extends Component {
 
             <Route exact path={Routes.CLASS} component={ClassPage} />
             {/* <Route path={Routes.CLASS_CREATE} component={} /> */}
-            {/* <Route path={Routes.CLASS_JOIN} component={} /> */}
+            <Route path={Routes.CLASS_JOIN} component={ClassJoinPage} />
             <Route path={Routes.CLASS_SETTINGS} component={ClassSettings} />
 
             <Route exact path={Routes.HOMEWORK} component={HomeworkPage} />
---
+++ client/src/components/pages/class/ClassJoinPage/ClassJoinPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ClassJoinPage extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default ClassJoinPage
