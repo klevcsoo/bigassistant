5aae2b4f1c503d362d0158f1fc1c7b0ab193c4a1
klevcsoo <kevelajsz@outlook.com>
2019-10-25 01:41:15 +0200

    Added click support

--- client/src/components/AppCard/AppCardClassContent.jsx
+++ client/src/components/AppCard/AppCardClassContent.jsx
@@ -5,7 +5,10 @@ import LocalizationHandler from '../../utils/LocalizationHandler'
 export class AppCardClassContent extends Component {
   render() {
     return (
-      <div className={`app-card app-card-class-content app-card-${this.props.type}`}>
+      <div className={`app-card app-card-class-content app-card-${this.props.type}`}
+      onClick={() => {setTimeout(() => {
+        if (this.props.onOpen) this.props.onOpen();
+      }, 200)}}>
         <h1 id="content-title">{this.props.title}</h1>
         <p>Tantárgy: <span id="content-subject">{this.props.subject}</span></p>
         <p>Időpont: <span id="content-date">{LocalizationHandler.formatDate(this.props.date)}</span></p>
