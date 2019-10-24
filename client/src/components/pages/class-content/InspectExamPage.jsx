64c1611582dba360d029b1ca6c390a5963fdb6dd
klevcsoo <kevelajsz@outlook.com>
2019-10-24 23:35:38 +0200

    Added InspectExamPage

---
+++ client/src/components/pages/class-content/InspectExamPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class InspectExamPage extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default InspectExamPage
