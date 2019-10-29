35fd2f40283275aecb779264d8a84046610e0bd0
klevcsoo <kevelajsz@outlook.com>
2019-10-29 10:58:26 +0100

    Added dark mode initialization on launch

--- client/src/index.js
+++ client/src/index.js
@@ -6,9 +6,13 @@ import * as serviceWorker from './serviceWorker';
 
 import App from './App';
 import FirebaseHandler from './utils/FirebaseHandler';
+import AppColours from './constants/appColors';
 
 // Firebase app needs to be initalized before the virtual DOM is built.
 FirebaseHandler.initializeFirebase();
+
+// Set dark mode at launch, according to key in local storage.
+AppColours.setDarkModeEnabled(AppColours.getDarkModeEnabled(), true);
 
 ReactDOM.render(<App />,document.getElementById('root'));
 
