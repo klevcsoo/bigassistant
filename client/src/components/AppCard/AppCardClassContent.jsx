14ccb527d67fc73080cd013fa722b48f22deb9b5
klevcsoo <kevelajsz@outlook.com>
2019-11-07 21:31:28 +0100

    Changed style of class content cards

--- client/src/components/AppCard/AppCardClassContent.jsx
+++ client/src/components/AppCard/AppCardClassContent.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './AppCard.css'
 import LocalizationHandler from './../../utils/LocalizationHandler'
+import AppColours from '../../constants/AppColours'
 
 const AppCardClassContent = ({ type, onOpen, title, subject, date }) => {
   return (
-    <div>
-      <div className={`app-card app-card-class-content app-card-${type}`}
-      onClick={() => {setTimeout(() => { if (onOpen) onOpen(); }, 200)}}>
+    <div className={`app-card app-card-class-content`}
+    onClick={() => {setTimeout(() => { if (onOpen) onOpen(); }, 200)}}>
+      <div className="accent" style={{
+        background: type === 'exam' ? AppColours.EXAM : type === 'homework' ? AppColours.HOMEWORK : AppColours.MAIN
+      }}></div>
+      <div className="content">
         <h1 id="content-title">{title}</h1>
         <p>Tantárgy: <span id="content-subject">{subject}</span></p>
         <p>Időpont: <span id="content-date">{LocalizationHandler.formatDate(date)}</span></p>
